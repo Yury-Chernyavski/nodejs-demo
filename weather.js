417feeb5cbc842c5f01f7d89c13d0916834b7586
Yury Chernyavski <yurachernyavskiw@gmail.com>
2023-11-08 01:17:12 +0100

    Command line argumetns

---
+++ weather.js
@@ -0,0 +1,20 @@
+#!/user/bin/env node
+import { getArgs } from "./helpers/args.js";
+
+const initCLI = () => {
+  const args = getArgs(process.argv);
+  console.log(args);
+  if (args.h) {
+    // output help
+  }
+  if (args.s) {
+    //save city
+  }
+  if (args.t) {
+    //save token
+  }
+
+  // output weather
+};
+
+initCLI();
